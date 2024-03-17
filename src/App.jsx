e813d6cdb62a70ad37af4208901e009e002dc419
weas3ls <shabaneh.ahmad@gmail.com>
2024-03-17 16:30:10 -0700

    delete project functionality

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,17 @@ function App() {
         });
     }
 
+    function handleDeleteProject() {
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: undefined,
+            projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId),
+        }));
+    }
+
     const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-    let content = <SelectedProject project={selectedProject} />;
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
     if (projectsState.selectedProjectId === null)
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
