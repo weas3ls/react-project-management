de4889945c9ec48ba798391f067bc068c48f6596
weas3ls <shabaneh.ahmad@gmail.com>
2024-03-17 14:50:46 -0700

    use state to conditionally show content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevProjectsState => ({
+            ...prevProjectsState,
+            selectedProjectId: null,
+        }));
+    }
+
+    let content;
+
+    if (projectsState.selectedProjectId === null) content = <NewProject />;
+    else content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+
     return (
         <main className="h-screen my-8 flex gap-8 ">
-            <ProjectsSidebar />
-            <NoProjectSelected />
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+            {content}
         </main>
     );
 }
